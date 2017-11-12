d2030b5b5f11f74a0ebe668f0f9fbe7e57c7dddd
wb-cjw323074 <wb-cjw323074@alibaba-inc.com>
2017-11-12 15:28:26 +0800

    feat(brew): 添加 mackup

--- brew.sh
+++ brew.sh
@@ -5,3 +5,7 @@ brew tap railwaycat/emacsmacport
 brew cask install emacs-mac
 brew cask install anki
 brew cask install dash
+
+# 用于还原备份(https://github.com/lra/mackup#supported-storages)
+brew install mackup
+
