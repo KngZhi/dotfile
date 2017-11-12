5f9b3ebc2cd460554c0244177541aab58f8c8cf2
wb-cjw323074 <wb-cjw323074@alibaba-inc.com>
2017-11-12 17:21:44 +0800

    init files by using mackup

---
+++ .emacs.d/init.el
@@ -0,0 +1,71 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory commentss.
+(package-initialize)
+
+(tool-bar-mode -1)
+
+;; 关闭文件滑动控件
+(scroll-bar-mode -1)
+
+;; 显示行号
+(global-linum-mode 1)
+
+;; 更改光标的样式（不能生效，解决方案见第二集）
+(setq-default cursor-type 'bar)
+
+;; 解决中文输入闪屏
+(setq redisplay-dont-pause nil) 
+
+;; 关闭启动帮助画面
+(setq inhibit-splash-screen 1)
+
+;; 关闭缩进 (第二天中被去除)
+;; (electric-indent-mode -1)
+
+;; 更改显示字体大小 16pt
+;; http://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
+(set-face-attribute 'default nil :height 160)
+
+;; 快速打开配置文件
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
+(global-set-key (kbd "<f2>") 'open-init-file)
+;; 开启全局 Company 补全
+(global-company-mode t)
+
+(setq make-backup-files nil)
+
+;; 让备注的编辑也有语法高亮
+(require 'org)
+(setq org-src-fontify-natively t)
+
+;; recent-mode
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+(delete-selection-mode t)
+;; 无效
+(setq initial-frame-alist (quote ((fullscreen . max-image-size))))
+
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- README.md
+++
@@ -1,6 +0,0 @@
-# Introduciton
-
-## iTerm2
-
-brew install list
-- `fzf` is a general-purpose command-line fuzzy finder.
